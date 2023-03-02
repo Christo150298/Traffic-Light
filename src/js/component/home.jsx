088409f0068c5cf5ad013be8ee8bb3e61253c5a5
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-03-02 08:48:45 +0000

    project casi terminado

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -4,25 +4,20 @@ import { useState } from "react";
 const Home = () => {
 	const [ selectedColor, setSelectedColor] = useState('red'); 
 	return (
-		<div className="container">
-			<div className="traffic-light">
-				<div 
-				onClick={() => setSelectedColor("red")}
-				className={
-					"red Light" + (selectedColor === "red" ? " selected" : "")
-				}></div>
-				<div 
-				onClick={() => setSelectedColor("yellow")}
-				className={
-					"yellow Light" + (selectedColor === "yellow" ? " selected" : "")
-				}></div>
-				<div 
-				onClick={() => setSelectedColor("green")}
-				className={
-					"green Light" + (selectedColor === "green" ? " selected" : "")
-				}></div>
+		<body>
+			<div className="container">
+				<div className="traffic-light">
+					<div onClick={() => setSelectedColor("red")}
+					className={"red Light" + (selectedColor === "red" ? " selected" : "")}></div>
+					<div onClick={() => setSelectedColor("yellow")}
+					className={"yellow Light" + (selectedColor === "yellow" ? " selected" : "")}></div>
+					<div onClick={() => setSelectedColor("green")}
+					className={"green Light" + (selectedColor === "green" ? " selected" : "")}></div>
+				</div>
+				<div className="stick"></div>
+				<div className="floor"></div>
 			</div>
-		</div>
+		</body>
 	);
 };
 
