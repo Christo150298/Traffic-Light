32b30a20a306c4089398d3135b789c54274430d8
Christopher Álvarez <christopherjavier1998@gmail.com>
2023-03-03 11:09:22 +0000

    Avances

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -2,11 +2,11 @@ import React from "react";
 import { useState } from "react";
 
 const Home = () => {
-	const [ selectedColor, setSelectedColor] = useState(''); 
+	const [ selectedColor, setSelectedColor] = useState('red'); 
+	const [ addPurple, setAddPurple] = useState('false');
 	return (
 		<body>
-			<button className="button">Push</button>
-			<div className="container">
+			<div className="col">	
 				<div className="traffic-light">
 					<div onClick={() => setSelectedColor("red")}
 					className={"red Light" + (selectedColor === "red" ? " selected" : "")}></div>
@@ -14,9 +14,15 @@ const Home = () => {
 					className={"yellow Light" + (selectedColor === "yellow" ? " selected" : "")}></div>
 					<div onClick={() => setSelectedColor("green")}
 					className={"green Light" + (selectedColor === "green" ? " selected" : "")}></div>
+					<div onClick={() => setAddPurple("purple")}
+					className={"purple Light" + (addPurple === "purple" ? " selected" : "")}></div>
 				</div>
 				<div className="stick"></div>
 				<div className="floor"></div>
+				<div>	
+					<button id="button" className="btn btn-dark">Push to change color</button>
+					<button id="purpleButtom" className="btn btn-dark">Push to add Purple</button>
+				</div>
 			</div>
 		</body>
 	);
